4310da311ee4e61e03f01bf750f978d52d231700
GowthamYadav1 <48523744+GowthamYadav1@users.noreply.github.com>
2026-07-07 01:18:39 +0530

    Created products.js

---
+++ products.js
@@ -0,0 +1,26 @@
+const products = [
+    { 
+        id: 1, 
+        name: "Neon Tetra", 
+        description: "Bright blue and red schooling community fish", 
+        price: 240,
+        image: "https://i.imgur.com/example1.jpg", // Switch to image path when ready
+        inStock: true 
+    },
+    { 
+        id: 2, 
+        name: "Guppy (Pair)", 
+        description: "Colorful, active, and easy to breed starter fish", 
+        price: 180,
+        image: "https://i.imgur.com/example2.jpg",
+        inStock: true
+    },
+    { 
+        id: 3, 
+        name: "Anubias Nana", 
+        description: "Hardy, low-light live aquarium plant", 
+        price: 150,
+        image: "https://i.imgur.com/example3.jpg",
+        inStock: false // Shows up as Out of Stock
+    }
+];
